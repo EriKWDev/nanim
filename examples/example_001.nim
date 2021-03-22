ab8e02f8492809b24dd3414544c4064af936d9fa
EriKWDev <ErikWDev@gmail.com>
2021-03-22 14:01:15 +0100

    Broken FFMpeg Renderer (works for short scenes?)

--- examples/example_001.nim
+++ examples/example_001.nim
@@ -3,36 +3,42 @@ import nanim
 
 
 proc testScene(): Scene =
-    let scene = newScene()
+  let scene = newScene()
 
-    var circle1 = newCircle(60)
-    var circle2 = newCircle(60)
+  var circle1 = newCircle(60)
+  var circle2 = newCircle(60)
 
-    var text = newText("Hello, World!")
+  var text = newText("Hello, World!")
 
-    var rect = newSquare()
+  var rect = newSquare()
 
-    scene.add(circle1, circle2, text, rect)
+  scene.add(circle1, circle2, text, rect)
 
-    scene.wait(1000)
-    scene.play(circle1.move(200, 200),
-               circle2.move(500, 200),
-               text.move(500, 500),
-               rect.move(100, 500),
-               rect.rotate(45))
+  discard circle1.move(150, 150)
 
-    scene.wait(1000)
-    scene.play(circle1.pscale(2),
-               circle2.scale(2),
-               rect.pscale(3))
+  scene.wait(400)
+  scene.play(circle1.move(200, 200),
+             circle2.move(500, 200),
+             text.move(500, 500),
+             rect.move(100, 500),
+             rect.rotate(45))
 
-    scene.wait(1000)
-    scene.play(rect.setTension(0.6))
+  scene.play(circle1.pscale(2),
+             circle2.scale(2),
+             rect.pscale(3))
 
-    scene.wait(1000)
+  scene.animate(rect.setTension(0.6))
 
-    return scene
+  scene.wait(500)
+
+  scene.play(circle1.pscale(1/2),
+             circle2.scale(1/2),
+             rect.pscale(1/3))
+
+  scene.wait(500)
+
+  return scene
 
 
 when isMainModule:
-    render(testScene())
+  render(testScene(), true)
