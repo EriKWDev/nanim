f975bbe5dfa7c9adf3a9e4cadae5e5d5c22c53ee
EriKWDev <ErikWDev@gmail.com>
2021-04-16 12:22:12 +0200

    Tween Options

--- src/nanim/entities.nim
+++ src/nanim/entities.nim
@@ -4,7 +4,9 @@ import
   nanim/entities/rectangle,
   nanim/entities/text,
   nanim/entities/engon,
-  nanim/entities/scene_entity
+  nanim/entities/scene_entity,
+  nanim/entities/point,
+  nanim/entities/line
 
 
-export circle, rectangle, text, engon, scene_entity
+export circle, rectangle, text, engon, scene_entity, point, line
