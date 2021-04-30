caf81fb01e4f3dd2bec5356b44a5d1329932f2ab
ErikWDev (new Mac) <ErikWDev@gmail.com>
2021-04-30 17:09:57 +0200

    Update

--- src/nanim/entities/point.nim
+++ src/nanim/entities/point.nim
@@ -7,31 +7,29 @@ import
 
 
 type
-  Point* = ref object of Entity
+  Dot* = ref object of Entity
     radius*: float
 
 
-proc init*(point: Point) =
+proc init*(point: Dot) =
   init(point.Entity)
   point.tension = 0
   point.radius = 10.0
 
 
-proc newPoint*(radius: float = 10.0): Point =
+proc newPoint*(radius: float = 10.0): Dot =
   new(result)
   result.init()
 
   result.radius = radius
 
 
-proc newDot*(radius: float = 10.0): Point = newPoint(radius)
-
-
+proc newDot*(radius: float = 10.0): Dot = newPoint(radius)
 proc newPivot*(): Entity = newEntity()
 proc newAnchor*(): Entity = newEntity()
 
 
-method draw*(point: Point, scene: Scene) =
+method draw*(point: Dot, scene: Scene) =
   let context = scene.context
   context.beginPath()
   context.circle(0, 0, point.radius)
