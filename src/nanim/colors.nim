f7b9dc6f3adb8d5fff0f67b1488b787b3bbeecfe
Erik <ErikWDev@gmail.com>
2021-05-10 18:01:48 +0200

    Fixed Color Parsing + Color Tests

--- src/nanim/colors.nim
+++ src/nanim/colors.nim
@@ -1,4 +1,4 @@
-import nanovg, re, tables, strutils, nanim/logging, sequtils
+import nanovg, nre, tables, strutils, nanim/logging, sequtils
 
 
 let colorsCache = newTable[string, Color]()
@@ -8,52 +8,52 @@ proc cacheColor(name: string, color: Color): Color {.inline.} =
   return color
 
 
-proc newColor*(r, g, b, a: float = 1.0): Color {.inline.} =
-  discard
+proc newColor*(r, g, b: int, a: float = 1.0): Color {.inline.} =
+  return cacheColor("rgb(" & $r & "," & $g & "," & $b & "," & $a & ")", rgba(r.float/255.0, g.float/255.0, b.float/255.0, a))
 
 
 let
   hexRegex = re("[#]?([0-9a-fA-F]{2})")
-  rgbRegex = re("""rgb\([\s]*?([+-]?[0-9]*[.]?[0-9]+)[\s]*?,[\s]*?([+-]?[0-9]*[.]?[0-9]+)[\s]*?,[\s]*?([+-]?[0-9]*[.]?[0-9]+)[\s]*?\)""")
-  rgbaRegex = re("""rgba\([\s]*?([+-]?[0-9]*[.]?[0-9]+)[\s]*?,[\s]*?([+-]?[0-9]*[.]?[0-9]+)[\s]*?,[\s]*?([+-]?[0-9]*[.]?[0-9]+)[\s]*?,[\s]*?([+-]?[0-9]*[.]?[0-9]+)[\s]*?\)""")
+  rgbRegex = re("([+-]?[0-9]*[.]?[0-9]+)")
 
 
 func hexToFloat(hexString: string): float {.inline.} =
-  return fromHex[int](hexString).toFloat()/255.0
+  return fromHex[int](hexString).toFloat()
 
 
 proc newColor*(cssString: string): Color =
   if colorsCache.hasKey(cssString):
     return colorsCache[cssString]
 
-  let hexMatch = cssString.findAll(hexRegex)
-  if len(hexMatch) == 3:
-      return cacheColor(cssString, rgb(hexToFloat(hexMatch[0]),
-                                       hexToFloat(hexMatch[1]),
-                                       hexToFloat(hexMatch[2])))
-  if len(hexMatch) >= 4:
-      return cacheColor(cssString, rgba(hexToFloat(hexMatch[0]),
-                                        hexToFloat(hexMatch[1]),
-                                        hexToFloat(hexMatch[2]),
-                                        hexToFloat(hexMatch[3])))
+  if cssString.contains("rgb"):
+    let rgbaMatch = cssString.findAll(rgbRegex)
+    if len(rgbaMatch) == 4:
+      return cacheColor(cssString, rgba(parseFloat(rgbaMatch[0])/255.0,
+                                        parseFloat(rgbaMatch[1])/255.0,
+                                        parseFloat(rgbaMatch[2])/255.0,
+                                        parseFloat(rgbaMatch[3])))
 
-  let rgbMatch = cssString.findAll(rgbRegex)
-  if len(rgbMatch) == 3:
-      return cacheColor(cssString, rgb(hexToFloat(rgbMatch[0]),
-                                       hexToFloat(rgbMatch[1]),
-                                       hexToFloat(rgbMatch[2])))
+    elif len(rgbaMatch) == 3:
+      return cacheColor(cssString, rgb(parseFloat(rgbaMatch[0])/255.0,
+                                       parseFloat(rgbaMatch[1])/255.0,
+                                       parseFloat(rgbaMatch[2])/255.0))
 
-  let rgbaMatch = cssString.findAll(rgbaRegex)
-  if len(rgbaMatch) == 4:
-      return cacheColor(cssString, rgba(hexToFloat(rgbaMatch[0]),
-                                        hexToFloat(rgbaMatch[1]),
-                                        hexToFloat(rgbaMatch[2]),
-                                        hexToFloat(rgbaMatch[3])))
+  else:
+    let hexMatch = cssString.findAll(hexRegex)
+    if len(hexMatch) == 3:
+      return cacheColor(cssString, rgb(hexToFloat(hexMatch[0])/255.0,
+                                       hexToFloat(hexMatch[1])/255.0,
+                                       hexToFloat(hexMatch[2])/255.0))
+    elif len(hexMatch) >= 4:
+      return cacheColor(cssString, rgba(hexToFloat(hexMatch[0])/255.0,
+                                        hexToFloat(hexMatch[1])/255.0,
+                                        hexToFloat(hexMatch[2])/255.0,
+                                        hexToFloat(hexMatch[3])/255.0))
+
 
 
   warn "'" & cssString & "' is not yet a supported/valid color format."
-
-  return rgb(0,0,0)
+  return rgba(0,0,0,0)
 
 
 func colorsFromCoolors*(link: string): seq[Color] {.inline.} =
--- tests/utils.nim
+++ tests/utils.nim
@@ -1,8 +1,8 @@
-import glm
+import glm, nanovg
 
 const Tolerance = 1e-8
 
-proc `~=`*(a, b: float): bool =
+proc `~=`*(a, b: float): bool {.inline.} =
   ## Check if "a" and "b" are close.
   ## We use a relative tolerance to compare the values.
 
@@ -10,14 +10,17 @@ proc `~=`*(a, b: float): bool =
   # echo $a & " ~= " & $b & " = " & $result
 
 
-proc `~=`*(a, b: Vec3[float]): bool =
+proc `~=`*(a, b: Vec3[float]): bool {.inline.} =
   result = a.x ~= b.x and a.y ~= b.y and a.z ~= b.z
 
-proc `~=`*(a, b: Vec2[float]): bool =
+proc `~=`*(a, b: Vec2[float]): bool {.inline.} =
   result = a.x ~= b.x and a.y ~= b.y
 
-proc `~=`*(a: Vec3[float], b: Vec2[float]): bool =
+proc `~=`*(a: Vec3[float], b: Vec2[float]): bool {.inline.} =
   result = a.x ~= b.x and a.y ~= b.y
 
-proc `~=`*(a: Vec2[float], b: Vec3[float]): bool =
+proc `~=`*(a: Vec2[float], b: Vec3[float]): bool {.inline.} =
   result = a.x ~= b.x and a.y ~= b.y
+
+proc `~=`*(a, b: Color): bool {.inline.} =
+  result = a.r ~= b.r and a.g ~= b.g and a.b ~= b.b and a.a ~= b.a
