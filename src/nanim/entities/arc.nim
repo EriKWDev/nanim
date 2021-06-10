5914f43aa4fa154108071e77ab96a90791a26356
Erik <ErikWDev@gmail.com>
2021-06-10 23:18:04 +0200

    Arc animations

--- src/nanim/entities/arc.nim
+++ src/nanim/entities/arc.nim
@@ -2,7 +2,8 @@
 import
   math,
   nanovg,
-  nanim/core
+  nanim/core,
+  nanim/animation
 
 
 type
@@ -30,10 +31,57 @@ proc newArc*(radius: float = 200.0, startAngle: float = 0.0, endAngle: float = 1
   result.direction = if clockWise: pwCW else: pwCCW
 
 
-
 method draw*(arcEntity: Arc, scene: Scene) =
   let context = scene.context
   context.beginPath()
   context.arc(0, 0, arcEntity.radius, arcEntity.startAngle, arcEntity.endAngle, arcEntity.direction)
   # context.closePath()
   scene.applyStyle(arcEntity.style)
+
+
+proc endAngleTo*(arc: Arc, endAngle: float = 0.0, mode: AngleMode = defaultAngleMode): Tween {.discardable.} =
+  var interpolators: seq[proc(t: float)]
+
+  let
+    angle =
+      case mode:
+      of amDegrees: math.degToRad(endAngle)
+      of amRadians: endAngle
+    startValue = arc.endAngle.deepCopy()
+    endValue = angle
+
+  let interpolator = proc(t: float) =
+    arc.endAngle = interpolate(startValue, endValue, t)
+
+  interpolators.add(interpolator)
+
+  arc.endAngle = endValue
+
+  result = newTween(interpolators)
+
+proc endAngleTo*(entities: openArray[Arc], endAngle: float = 0.0, mode: AngleMode = defaultAngleMode): seq[Tween] {.discardable.} =
+  for i in 0..high(entities):
+    result.add(entities[i].endAngleTo(endAngle, mode))
+
+
+proc startAngleTo*(entity: Arc, startAngle: float = 0.0, mode: AngleMode = defaultAngleMode): Tween {.discardable.} =
+  var interpolators: seq[proc(t: float)]
+
+  let
+    angle =
+      case mode:
+      of amDegrees: math.degToRad(startAngle)
+      of amRadians: startAngle
+    startValue = entity.startAngle.deepCopy()
+    endValue = angle
+
+  let interpolator = proc(t: float) =
+    entity.startAngle = interpolate(startValue, endValue, t)
+
+  interpolators.add(interpolator)
+  entity.startAngle = endValue
+  result = newTween(interpolators)
+
+proc startAngleTo*(entities: openArray[Arc], startAngle: float = 0.0, mode: AngleMode = defaultAngleMode): seq[Tween] {.discardable.} =
+  for i in 0..high(entities):
+    result.add(entities[i].startAngleTo(startAngle, mode))
